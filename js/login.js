b0d76db66ab2659670180f707cc12e5637dab55e
Davi <desenvolvimento2@portalabre.com.br>
2022-11-07 17:59:32 -0300

    Correções solicitadas para reavaliação do projeto

--- js/login.js
+++ js/login.js
@@ -1,57 +1,80 @@
 const url = "https://reqres.in/api/login"
 let senha = document.querySelector('#pass')
 let nome = document.querySelector('#user')
+let titleErrors = document.querySelector('#error')
+let button = document.querySelector('#loginBtn')
 let visibilitySearch = document.querySelector('#search')
 let visibilityLogin = document.querySelector('#login')
 let validSenha = false;
 let validNome = false;
 
+senha.value='';
+nome.value='';
+button.disabled = true;
+
+
 //Validação dos Campos de senha e userId
 nome.addEventListener('keyup', () => {
-    if (nome.value.length <= 3) {
+    if (nome.value.length < 3) {
         nome.setAttribute('style', 'border: solid red')
         validNome = false;
+        button.disabled = true;
+
     } else {
         nome.setAttribute('style', 'none')
         validNome = true;
+
     }
-})
+});
 
 senha.addEventListener('keyup', () => {
-    if (senha.value.length <= 3) {
+    if (senha.value.length < 3) {
         senha.setAttribute('style', 'border: solid red')
         validSenha = false;
+        button.disabled = true;
     } else {
         senha.setAttribute('style', 'none')
         validSenha = true;
+        button.disabled = false;
     }
-})
+});
 
-function validaLogin(){
+button.addEventListener('click', () => {
+    if (validNome === false || validSenha === false) {
+        button.disabled = true;
+    } else {
+        titleErrors.innerHTML = '';
+        button.disabled = false;
+    }
+});
+
+
+function validaLogin() {
     const id = localStorage.getItem("user")
-    if(id != null){
+    if (id != null) {
         visibilityLogin.classList.toggle('hidden')
         visibilitySearch.classList.toggle('hidden')
     }
 }
 
-function login(){
+function login() {
     const payloud = {
-        email:nome.value,
+        email: nome.value,
         password: senha.value
     };
     axios.post(url, payloud)
         .then(response => {
             localStorage.setItem("user", nome.value)
-            if(!!response.data){
+            if (!!response.data) {
                 visibilityLogin.classList.toggle('hidden')
                 visibilitySearch.classList.toggle('hidden')
-            }else{
-                alert('Não aprovado')
+            } else {
+                titleErrors.style.color = '#FF0000';
+                titleErrors.innerHTML = "Não aprovado"
             }
         })
-        .catch((error) =>{
-            alert('deu bad')
+        .catch((error) => {
+            titleErrors.innerHTML = 'Verifique os campos e tente novamente'
         })
 }
 
--- js/search.js
+++ js/search.js
@@ -1,23 +1,39 @@
-const carName = document.querySelector('#carName')
-const tableResult = document.querySelector('#result')
+let carName = document.querySelector('#carName')
+let tableResult = document.querySelector('#result')
+let errors = document.querySelector('#carNameError')
+let searchBtn = document.querySelector('#searchBtn')
 
-function consultar(){
+carName.value = '';
+searchBtn.disabled = true;
+
+carName.addEventListener('keyup', () => {
+    if (carName.value.length < 3) {
+        errors.innerHTML = 'Digite ao menos 3 letras';
+        searchBtn.disabled = true;
+    } else {
+        errors.innerHTML = '';
+        searchBtn.disabled = false;
+    }
+});
+
+
+function consultar() {
     var child = tableResult.lastElementChild;
     while (child) {
-        if(child.tagName !== "TBODY"){
+        if (child.tagName !== "TBODY") {
             tableResult.removeChild(child);
             child = tableResult.lastElementChild;
-        }else{
+        } else {
             break;
         }
     }
 
     $.ajax({
         method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/cars?model='+carName.value,
-        headers: { 'X-Api-Key': 'mLqNRQyW+lTu/RWiZfCnRg==iEumzB29d7n7NmGD'},
+        url: 'https://api.api-ninjas.com/v1/cars?model=' + carName.value,
+        headers: {'X-Api-Key': 'mLqNRQyW+lTu/RWiZfCnRg==iEumzB29d7n7NmGD'},
         contentType: 'application/json',
-        success: function(result) {
+        success: function (result) {
             let data = result;
             for (let i = 0; i < data.length; i++) {
                 var tr = document.createElement('tr')
@@ -26,7 +42,6 @@ function consultar(){
                 var td3 = document.createElement('td')
                 td1.innerHTML = data[i].make
                 td2.innerHTML = data[i].year
-                td3.innerHTML = data[i].model
                 td3.innerHTML = data[i].model
                 tableResult.append(tr)
                 tr.appendChild(td1)
