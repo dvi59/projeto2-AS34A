afcc1eeca1c7c76e022eb60bcca6754cc48b7f78
Davi <desenvolvimento2@portalabre.com.br>
2022-12-19 20:38:17 -0300

    Projeto 3 - Programação Web

--- js/login.js
+++ js/login.js
@@ -1,16 +1,34 @@
 const url = "https://reqres.in/api/login"
 let senha = document.querySelector('#pass')
 let nome = document.querySelector('#user')
+let pass = document.querySelector('#passCad')
+let username = document.querySelector('#userCad')
+let email = document.querySelector('#emailCad')
 let titleErrors = document.querySelector('#error')
 let button = document.querySelector('#loginBtn')
 let visibilitySearch = document.querySelector('#search')
 let visibilityLogin = document.querySelector('#login')
+let visibilityRegister = document.querySelector('#register')
+let visibilityCarRegister = document.querySelector('#carRegister')
+let messageSucess = document.querySelector('#message-sucess')
+let messageCar = document.querySelector('#messageCar')
+let marca = document.querySelector("#carnameCad")
+let modelo = document.querySelector("#modeloCad")
+let ano = document.querySelector('#anoCad')
+let foto = document.querySelector('#fotoCad')
+let tipo = document.querySelector('#tipo')
+let registerBtn = document.querySelector('#registerBtnCar')
 let validSenha = false;
 let validNome = false;
+//let token = document.cookie
 
-senha.value='';
-nome.value='';
+
+senha.value = '';
+nome.value = '';
 button.disabled = true;
+pass.value = '';
+username.value = '';
+email.value = '';
 
 
 //Validação dos Campos de senha e userId
@@ -77,4 +95,98 @@ function login() {
             titleErrors.innerHTML = 'Verifique os campos e tente novamente'
         })
 }
+
+function register() {
+    visibilityLogin.classList.toggle('hidden')
+    visibilityRegister.classList.toggle('hidden')
+}
+
+function cadUser() {
+    console.log("Cadastrando...")
+    axios.post('http://localhost:1337/auth/register', {
+        name: username.value,
+        email: email.value,
+        password: pass.value,
+        tipo: tipo.value
+    }).then((res) => {
+        messageSucess.innerHTML = "Usuário Cadastrado com sucesso"
+        messageSucess.style.color = "green";
+        console.log("200OK", res)
+    })
+        .catch((error) => {
+            messageSucess.innerHTML = "Verifique os campos e tente novamente"
+            messageSucess.style.color = "red";
+        })
+}
+
+function cadCar() {
+    console.log("Cadastrando Vecículo")
+    const token = document.cookie.split('?').find((row) => row.startsWith('token='))?.split('=')[1]
+    console.log("TOKEN DISPARADO PARA CADASTRO",token)
+    axios.post('http://localhost:1337/car/register', {
+        name: marca.value,
+        modelo: modelo.value,
+        ano: ano.value,
+        foto: foto.value,
+    },{headers: {Authorization: token}}).then((res) => {
+        messageCar.innerHTML = "Veículo Cadastrado com sucesso"
+        messageCar.style.color = "green";
+        console.log("200OK", res)
+        console.log(res)
+    }).catch((error) => {
+        messageCar.innerHTML = ""
+        messageCar.style.color = "red";
+    })
+    marca.value = ''
+    modelo.value = ''
+    ano.value = ''
+    foto.value = ''
+    messageCar.innerHTML = ''
+}
+
+function loginMongo() {
+    axios.post('http://localhost:1337/auth/login', {
+        email: nome.value,
+        password: senha.value,
+    }).then((res) => {
+        visibilityLogin.classList.toggle('hidden')
+        visibilitySearch.classList.toggle('hidden')
+        console.log("Pegando tipo", res.data.tipo, res.data.token)
+        const token = res.data.token
+        const tipo = res.data.tipo
+        
+        document.cookie = "tipo="+tipo+"?token="+ token + ";";
+        const userTipo= document.cookie.split('?').find((row) => row.startsWith('tipo='))?.split('=')[1]
+        if (userTipo === 'notAdm'){
+            console.log("tipoUserValidation" >> userTipo)
+            registerBtn.classList.toggle('hidden')
+        }
+    
+    })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+function carRegister() {
+    visibilityCarRegister.classList.toggle('hidden')
+    visibilitySearch.classList.toggle('hidden')
+}
+
+
+function testeApi() {
+   
+    const token = document.cookie.split('?').find((row) => row.startsWith('token='))?.split('=')[1]
+    if (token) {
+        visibilityLogin.classList.toggle('hidden')
+        visibilitySearch.classList.toggle('hidden')
+    }
+    const userTipo= document.cookie.split('?').find((row) => row.startsWith('tipo='))?.split('=')[1]
+    if (userTipo === 'notAdm'){
+        console.log("tipoUserValidation" >> userTipo)
+        registerBtn.classList.toggle('hidden')
+    }
+    
+}
+
 
--- js/search.js
+++ js/search.js
@@ -2,6 +2,7 @@ let carName = document.querySelector('#carName')
 let tableResult = document.querySelector('#result')
 let errors = document.querySelector('#carNameError')
 let searchBtn = document.querySelector('#searchBtn')
+//let registerBtn = document.querySelector('#registerBtnCar')
 
 carName.value = '';
 searchBtn.disabled = true;
@@ -27,32 +28,36 @@ function consultar() {
             break;
         }
     }
-
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/cars?model=' + carName.value,
-        headers: {'X-Api-Key': 'mLqNRQyW+lTu/RWiZfCnRg==iEumzB29d7n7NmGD'},
-        contentType: 'application/json',
-        success: function (result) {
-            let data = result;
-            for (let i = 0; i < data.length; i++) {
-                var tr = document.createElement('tr')
-                var td1 = document.createElement('td')
-                var td2 = document.createElement('td')
-                var td3 = document.createElement('td')
-                td1.innerHTML = data[i].make
-                td2.innerHTML = data[i].year
-                td3.innerHTML = data[i].model
-                tableResult.append(tr)
-                tr.appendChild(td1)
-                tr.appendChild(td2)
-                tr.appendChild(td3)
+   
+    const token = document.cookie.split('?').find((row) => row.startsWith('token='))?.split('=')[1]
+    console.log("TOKEN DISPARADO PARA CONSULTA",token)
+    axios.get('http://localhost:1337/car/search', {  headers: {Authorization: token}})
+        .then((res) => {
+            
+            console.log("Rodou",res.data.car[2])
+            for (let i = 0; i < res.data.car.length; i++) {
+                console.log("Entrou no laço")
+                if (carName.value === res.data.car[i].name) {
+                    console.log("Entrou no if")
+                    var tr = document.createElement('tr')
+                    var td1 = document.createElement('td')
+                    var td2 = document.createElement('td')
+                    var td3 = document.createElement('td')
+                    td1.innerHTML = res.data.car[i].name
+                    td2.innerHTML = res.data.car[i].ano
+                    td3.innerHTML = res.data.car[i].modelo
+                    tableResult.append(tr)
+                    tr.appendChild(td1)
+                    tr.appendChild(td2)
+                    tr.appendChild(td3)
+                }
             }
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
-    });
+        }).catch((error) => {
+            console.log("ERRO EM CONSUMIR DADOS", error)
+        })
 }
+
+
+
 
 
